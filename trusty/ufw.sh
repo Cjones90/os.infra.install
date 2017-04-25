54ac643f57f82a6795ff98412aa1e1d75fa29c2d
KC <kcjones.2002@gmail.com>
2017-04-25 14:08:11 -0700

    Allow all docker-compose subnets that are also not publically routable on global internet

--- trusty/ufw.sh
+++ trusty/ufw.sh
@@ -21,6 +21,20 @@ if [[ ! $PG = true ]] && [[ ! $MONGO = true ]]; then
     sudo ufw allow to any port 2377 proto tcp           # Docker
     sudo ufw allow to any port 7946 proto tcp           # Docker
     sudo ufw allow to any port 7946 proto udp           # Docker
+
+    # https://www.arin.net/knowledge/address_filters.html
+    # According to standards set forth in Internet Engineering Task Force (IETF)
+    # document RFC-1918, the following IPv4 address ranges have been reserved by the
+    # IANA for private internets, and are not publicly routable on the global internet:
+    #   10.0.0.0/8 IP addresses: 10.0.0.0 -- 10.255.255.255
+    #   172.16.0.0/12 IP addresses: 172.16.0.0 -- 172.31.255.255
+    #   192.168.0.0/16 IP addresses: 192.168.0.0 – 192.168.255.255
+
+    # https://github.com/moby/moby/pull/29376
+    # When doing `docker-compose up`, below are dockers hardcoded subnet pools it
+    # pulls from (for now)
+    sudo ufw allow from 172.16.0.0/12                   # Docker-compose subnets
+    sudo ufw allow from 192.168.0.0/20                  # Docker-compose subnets
 fi
 
 # Allow ips based on server role
